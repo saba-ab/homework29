c0d03f8a7c07ef505dc2afac5fd0a49394cbe693
saba-ab <sabaabjandadze@gmail.com>
2023-06-12 06:47:32 +0400

    added homework 29

--- script.js
+++ script.js
@@ -31,3 +31,96 @@ const textCharCode = text.charCodeAt(7);
 console.log(textCharCode);
 const splitedText = text.split(" ");
 console.log(splitedText);
+
+// 2
+let commentsArr = [
+  {
+    postId: 1,
+    id: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz",
+    body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikita@garfield.biz",
+    body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione",
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body: "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati",
+  },
+  {
+    postId: 1,
+    id: 5,
+    name: "vero eaque aliquid doloribus et culpa",
+    email: "Hayden@althea.biz",
+    body: "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et",
+  },
+];
+
+let commData = {};
+
+const commForm = document.getElementById("commentForm");
+commForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputs = document.querySelectorAll(`input, textarea`);
+  inputs.forEach((input) => {
+    commData[input.name] = input.value.trim();
+    if (input.value.trim() == " ") {
+      const error = document.createElement("p");
+      error.setAttribute("class", "error");
+      error.textContent = "Please fill all inputs";
+      commForm.appendChild(error);
+    }
+  });
+  const keys = Object.keys(commData);
+  const lastKey = keys.pop();
+  delete commData[lastKey];
+  commentsArr.push(commData);
+  commForm.reset();
+  commData = {};
+});
+const section = document.createElement("section");
+document.body.appendChild(section);
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+section.appendChild(container);
+
+for (let i = 0; i < commentsArr.length; i++) {
+  let user = commentsArr[i];
+
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  container.appendChild(card);
+  const name = document.createElement("p");
+  name.setAttribute("class", "bold");
+  const email = document.createElement("a");
+  email.setAttribute("href", `mailto:user.email`);
+  const bodyText = document.createElement("p");
+  bodyText.setAttribute("class", "light");
+  name.textContent = user.name;
+  email.textContent = user.email;
+  bodyText.textContent = user.body;
+  card.appendChild(name);
+  card.appendChild(email);
+  card.appendChild(bodyText);
+  const remove = document.createElement("img");
+  remove.setAttribute("src", "./media/trash-solid.svg");
+  card.appendChild(remove);
+  remove.addEventListener("click", () => {
+    card.remove();
+  });
+}
